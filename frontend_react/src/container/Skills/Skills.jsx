396e0dd7cecd24333057a48f96e635ede687d406
Sandeep <sandeepiitm2003@gmail.com>
2023-01-08 12:21:26 +0530

    added jsx to the experience section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React , {useState, useEffect} from 'react';
 import {motion} from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
+import {Tooltip as ReactTooltip} from 'react-tooltip';
 
 import {AppWrap} from '../../wrapper';
 import {urlFor, client} from '../../client';
@@ -33,7 +33,7 @@ const Skills = () => {
 
       <div className='app__skills-container'>
         <motion.div className='app__skills-list'>
-          {skills.map((skill) =>(
+          {skills?.map((skill) =>(
             <motion.div
               whileInView={{opacity:[0, 1]}}
               whileHover={{scale: 1.1}}
@@ -50,11 +50,52 @@ const Skills = () => {
           ))}
 
         </motion.div>
+        <motion.div className='app__skills-exp'>
+          {experience?.map((experience) => (
+            <motion.div
+            className='app__skills-exp-item'
+            key={experience.year}
+            > 
+              <div className='app__skills-exp-year'>
+                <p className='bold-text'>{experience.year}</p>
+              </div>
+              <motion.div className='app__skills-exp-works'>
+                {experience.works.map((work) =>(
+                  <>
+                  <motion.dvi
+                  whileInView={{opacity:[0, 1]}}
+                  whileHover={{scale: 1.1}}
+                  transition = {{duration: 0.1, type: 'tween'}}
+                  className = "app__skills-exp-work app__flex"
+                  data-tip 
+                  data-for = {work.name}
+                  key = {work.name}
+                  >
+                    <h4 className='bold-text'>{work.name}</h4>
+                    <p className='p-text'>{work.company}</p>
+                  </motion.dvi>
+                  <ReactTooltip
+                  id = {work.name}
+                  effect = "solid"
+                  arrowColor = '#fff'
+                  className = "skills-tooltip"
+                  >
+                    {work.desc}
+      
+                  </ReactTooltip>
+                  </>
+                  
+                ))}
+              </motion.div>
+
+            </motion.div>
+            
+          ))}
+            
+
+        </motion.div>
 
       </div>
-
-
-
     </>
     
   )
