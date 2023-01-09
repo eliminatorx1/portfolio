9c0b15c7275eb03caeb14d57de11933bf3f0ed02
Sandeep <sandeepiitm2003@gmail.com>
2023-01-09 16:34:35 +0530

    added the testimonial section

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,31 +1,31 @@
 export default{
-    name: "testimonials",
-    title: "Testimonials",
-    type:"document",
+    name: 'testimonials',
+    title: 'testimonials',
+    type:'document',
     fields:[
         {
-            name:"name",
-            title:"Name",
-            type:"string"
+            name:'name',
+            title:'Name',
+            type:'string'
         },
         {
-            name:"company",
-            title:"Company",
-            type:"string"
+            name:'company',
+            title:'Company',
+            type:'string'
         },
         {
-            name:"imageurl",
-            title:"ImgURL",
-            type:"image",
+            name:'imgurl',
+            title:'ImgUrl',
+            type:'image',
             Options: {
                 hotspot:true, //by using this feature the user will be able to crop his/her image, without the use of sanity we have to implement this feature from the scratch
-            }
+            },
         },
         {
-            name:"feedback",
-            title:"Feedback",
-            type:"string"
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
         },
 
-    ],
+    ]
 }
--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react';
+import {motion} from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+
+import {AppWrap, MotionWrap} from '../../wrapper';
+import {urlFor, client} from '../../client';
 
 import './Testimonial.scss';
 
 const Testimonial = () => {
+
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsquery = '*[_type == "brands"]'; //fectching the data from the sanity client
+    
+    client.fetch(query)
+      .then((data) =>{
+        setTestimonials(data);
+      });
+    client.fetch(brandsquery)
+      .then((data) =>{
+        setBrands(data);
+      });
+  
+  }, []);
+
   return (
-    <div>Testimonial</div>
+    <>
+    {testimonials.length &&(
+      <>
+        <div className='app__testimonial-item app__flex'>
+        <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
+        {console.log("this is the debug" ,urlFor(testimonials[currentIndex].imgurl).url())}
+          <div className='app__testimonial-content'>
+            <p className='p-text'>{testimonials[currentIndex].feedback}</p>
+          </div>
+        </div>
+      </>
+
+    )}
+
+
+    </>
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  "app__primarybg"
+  );
